fa4508e321bc79078898c0bc6c60a1229629c0f9
Guillaume Rodriguez <guillaume.rodriguez@epita.fr>
2024-08-06 14:54:32 +0200

    statut neutre de like

--- app/api/like/route.ts
+++ app/api/like/route.ts
@@ -3,41 +3,54 @@ import { createClient } from "@/utils/supabase/client";
 import { SupabaseClient } from "@supabase/supabase-js";
 import { NextRequest, NextResponse } from "next/server";
 
-async function isAlreadyLiked(
+async function destroyLike(
   supabase: SupabaseClient,
-  user_id: number,
+  user_id: string,
+  word_id: number
+) {
+  const { data, error } = await supabase
+    .from("like")
+    .delete()
+    .eq("user", user_id)
+    .eq("word", word_id);
+  if (error) return error;
+  return data;
+}
+
+async function hasToChange(
+  supabase: SupabaseClient,
+  user_id: string,
   word_id: number,
-  like: boolean
+  like: boolean | null
 ) {
   const { data, error } = await supabase
     .from("like")
     .select("*")
     .eq("user", user_id)
     .eq("word", word_id)
-    .eq("like", like)
     .single();
   if (error || !data) return false;
-  if (data.length === 0) return false;
+  if (data.like === like) return false;
   return true;
 }
 
 function parseParamsPOST(request: NextRequest): {
   id?: number;
-  like?: boolean;
+  like: boolean | null;
   error?: string;
 } {
   const searchParams = request.nextUrl.searchParams;
   const id = searchParams.get("id");
   const like = searchParams.get("like");
 
-  if (!id) return { error: "Please set an id and like" };
+  if (!id) return { like: null, error: "Please set an id" };
 
   const idNumber = Number(id);
   if (Number.isNaN(idNumber) || idNumber <= 0)
     return { error: "id number should be positive" };
 
-  if (like !== "true" && like !== "false")
-    return { error: "like should be a boolean" };
+  if (like !== null && like !== "true" && like !== "false")
+    return { error: "like should be a boolean or null" };
   const likeBool = Boolean(like);
   return { id: idNumber, like: likeBool };
 }
@@ -48,13 +61,16 @@ export async function POST(request: NextRequest) {
   const supabase = createClient();
 
   const { id, like, error } = parseParamsPOST(request);
-  if (error || !id || !like) return NextResponse.json({ error });
+  if (error || !id) return NextResponse.json({ error });
 
   const user_id = await getProfile(supabase);
-  if (user_id.error) return NextResponse.json({ error: user_id.error });
+  if (user_id.error || !user_id.data)
+    return NextResponse.json({ error: user_id.error });
 
-  if (await isAlreadyLiked(supabase, user_id.data.id, id, like))
-    return NextResponse.json({ error: "Already liked" });
+  if (await hasToChange(supabase, user_id.data.id, id, like))
+    return NextResponse.json({ error: "Already in this state" });
+  else if (await destroyLike(supabase, user_id.data.id, id))
+    return NextResponse.json({ error: "Error when deleting previous like" });
 
   const insert = await supabase.from("like").insert({
     user: user_id.data.id,
