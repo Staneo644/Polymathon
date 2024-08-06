2fbee4aa5fe3c690fc017926b63b6e80bb584e9e
Guillaume Rodriguez <guillaume.rodriguez@epita.fr>
2024-08-06 18:04:17 +0200

    fdkjfldkfj

--- components/listCard.tsx
+++ components/listCard.tsx
@@ -1,47 +1,58 @@
-import { useState, useRef, useEffect } from 'react';
-import oneCard from './oneCard';
+import { useState, useRef, useEffect } from "react";
+import oneCard from "./oneCard";
 //import type { word_id } from './entity';
-import { completeWord } from '@/app/api/word/route';
+import { completeWord } from "@/app/api/word/route";
 //import { seeWord } from '../communication/word';
 // import '../globals.css'
 
 let wordIndex = -1;
 let cardIndex = 0;
 let oldWordIndex = -1;
-function listCardComponent(liste: completeWord[], setList:(word:completeWord[])=>void,onEnd: null | (() => void)) {
+function listCardComponent(
+  liste: completeWord[],
+  setList: (word: completeWord[]) => void,
+  onEnd: null | (() => void)
+) {
   const containerRefMiddle = useRef<HTMLDivElement | null>(null);
   const [isMouseDown, setIsMouseDown] = useState(false);
   const [yMouseDown, setYMouseDown] = useState(0);
   const [yCoord, setYCoord] = useState(0);
   const [listCard, setListCard] = useState<oneCard[]>([]);
-  const login = typeof localStorage !== 'undefined' ? localStorage.getItem('access_token') : '';
-
+  const login =
+    typeof localStorage !== "undefined"
+      ? localStorage.getItem("access_token")
+      : "";
 
   const onChangeWord = (word: completeWord) => {
     liste[wordIndex] = word;
     console.log(liste);
     setYCoord(yCoord + 1);
     setList(liste);
-  }
+  };
 
   const checked = (): boolean => {
     return liste && liste.length > 0;
   };
 
-  const seeingWord = (word:completeWord) => {
-    //seeWord(word.id)
+  const seeingWord = (word: completeWord) => {
+    //seeWord(word.id) TODO
     oldWordIndex = wordIndex;
-  }
-  
+  };
+
   useEffect(() => {
-    if (login && login !== '' && oldWordIndex < wordIndex && listCard[cardIndex]) {
+    if (
+      login &&
+      login !== "" &&
+      oldWordIndex < wordIndex &&
+      listCard[cardIndex]
+    ) {
       seeingWord(listCard[cardIndex].getWord());
     }
   }, [wordIndex]);
 
   const rebootListCard = () => {
     listCard.forEach((oneCard: oneCard) => {
-      oneCard.setTransition('0.5s');
+      oneCard.setTransition("0.5s");
       oneCard.setPosition(0);
       setYCoord(yCoord + 1);
     });
@@ -62,11 +73,11 @@ function listCardComponent(liste: completeWord[], setList:(word:completeWord[])=
         addElementAtEnd(
           new oneCard({
             word: liste[wordIndex + 1],
-            refPosition: '150%',
-            transition: '0.5s',
+            refPosition: "150%",
+            transition: "0.5s",
             _index: wordIndex + 1,
             onChange: onChangeWord,
-          }),
+          })
         );
       }
       setRefPosition();
@@ -85,11 +96,11 @@ function listCardComponent(liste: completeWord[], setList:(word:completeWord[])=
         addElementAtStart(
           new oneCard({
             word: liste[wordIndex - 1],
-            refPosition: '-50%',
-            transition: '0.5s',
+            refPosition: "-50%",
+            transition: "0.5s",
             _index: wordIndex - 1,
             onChange: onChangeWord,
-          }),
+          })
         );
       }
       setRefPosition();
@@ -103,21 +114,21 @@ function listCardComponent(liste: completeWord[], setList:(word:completeWord[])=
         ret.push(
           new oneCard({
             word: liste[0],
-            refPosition: '50%',
-            transition: '0',
+            refPosition: "50%",
+            transition: "0",
             _index: 0,
             onChange: onChangeWord,
-          }),
+          })
         );
         if (liste.length > 1) {
           ret.push(
             new oneCard({
               word: liste[1],
-              refPosition: '150%',
-              transition: '0',
+              refPosition: "150%",
+              transition: "0",
               _index: 1,
               onChange: onChangeWord,
-            }),
+            })
           );
         }
       }
@@ -130,30 +141,30 @@ function listCardComponent(liste: completeWord[], setList:(word:completeWord[])=
   }, [liste]);
 
   const setRefPosition = (): void => {
-    listCard[cardIndex].setRefPosition('50%');
+    listCard[cardIndex].setRefPosition("50%");
     if (cardIndex < listCard.length - 1) {
-      listCard[cardIndex + 1].setRefPosition('150%');
+      listCard[cardIndex + 1].setRefPosition("150%");
     }
     if (cardIndex > 0) {
-      listCard[cardIndex - 1].setRefPosition('-50%');
+      listCard[cardIndex - 1].setRefPosition("-50%");
     }
   };
 
   useEffect(() => {
     const handleKeyPress = (e: any) => {
-      if (e.key === 'ArrowLeft') {
+      if (e.key === "ArrowLeft") {
         rebootListCard();
         moveToLeft();
-      } else if (e.key === 'ArrowRight') {
+      } else if (e.key === "ArrowRight") {
         rebootListCard();
         moveToRight();
       }
     };
 
-    window.addEventListener('keydown', handleKeyPress);
+    window.addEventListener("keydown", handleKeyPress);
 
     return () => {
-      window.removeEventListener('keydown', handleKeyPress);
+      window.removeEventListener("keydown", handleKeyPress);
     };
   }, [moveToLeft, moveToRight]);
 
@@ -183,7 +194,7 @@ function listCardComponent(liste: completeWord[], setList:(word:completeWord[])=
 
   const handleMouseDown = (event: any): void => {
     listCard.forEach((oneCard: any) => {
-      oneCard.setTransition('0s');
+      oneCard.setTransition("0s");
     });
     setYMouseDown(event.clientX);
     setIsMouseDown(true);
@@ -212,7 +223,7 @@ function listCardComponent(liste: completeWord[], setList:(word:completeWord[])=
 
   return (
     <>
-      {' '}
+      {" "}
       {!checked() && (
         <div className="text-white">
           Liste de mots vide, vérifiez votre connexion
--- components/oneCard.tsx
+++ components/oneCard.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { note } from './entity';
-import EtymologyComponent from './etymology';
-import { faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { completeWord } from '@/app/api/word/route';
+import React from "react";
+import { note } from "./entity";
+import EtymologyComponent from "./etymology";
+import { faThumbsUp, faThumbsDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { completeWord } from "@/app/api/word/route";
 //import { noteWord } from '../communication/word';
 
 class OneCard extends React.Component {
@@ -31,6 +31,7 @@ class OneCard extends React.Component {
   onChange: (word: completeWord) => void;
 
   noteMyWord = async (myNote: note): Promise<void> => {
+    console.log(myNote);
     /*noteWord(this.word.id, myNote).then((note_ret) => {
       console.log(this.word.personnal_note)
       if (myNote === note.positif ) {
@@ -61,7 +62,7 @@ class OneCard extends React.Component {
       this.word.personnal_note = note_ret;
       this.onChange(this.word);
     })*/
-  }
+  };
 
   getIndex(): number {
     return this.index;
@@ -88,20 +89,20 @@ class OneCard extends React.Component {
       <div
         className="text-black overflow-y-scroll flex flex-col"
         style={{
-          width: '85vw',
-          justifyContent: 'center',
-          alignItems: 'center',
+          width: "85vw",
+          justifyContent: "center",
+          alignItems: "center",
           top: `calc(58px + 5%)`,
           backgroundImage:
-            'url(https://s2.qwant.com/thumbr/474x323/7/5/15e7a9bcd784af960fb05e85addd943f5f08a5259bb5803b58a1b3f39473cc/th.jpg?u=https%3A%2F%2Ftse.mm.bing.net%2Fth%3Fid%3DOIP.jytUH6XTOQ7pXAgURy6LYQHaFD%26pid%3DApi&q=0&b=1&p=0&a=0)',
-          backgroundSize: 'cover',
+            "url(https://s2.qwant.com/thumbr/474x323/7/5/15e7a9bcd784af960fb05e85addd943f5f08a5259bb5803b58a1b3f39473cc/th.jpg?u=https%3A%2F%2Ftse.mm.bing.net%2Fth%3Fid%3DOIP.jytUH6XTOQ7pXAgURy6LYQHaFD%26pid%3DApi&q=0&b=1&p=0&a=0)",
+          backgroundSize: "cover",
           left: `calc(${this.refPosition} + ${this.position}px)`,
-          transform: 'translate(-50%)',
-          borderRadius: '20px',
-          boxShadow: '0px 0px 10px 0px black',
-          overflowY: 'scroll',
-          overflowX: 'hidden',
-          position: 'fixed',
+          transform: "translate(-50%)",
+          borderRadius: "20px",
+          boxShadow: "0px 0px 10px 0px black",
+          overflowY: "scroll",
+          overflowX: "hidden",
+          position: "fixed",
           transition: this.transition,
         }}
       >
@@ -113,42 +114,40 @@ class OneCard extends React.Component {
             {this.word.name}
           </h3>
           <h4 className="text-lg text-gray-600 italic">
-            {'  ('}
+            {"  ("}
             {this.word.type}
-            {')'}
+            {")"}
           </h4>
         </div>
         <div className="mb-2 ml-2 text-xl italic font-serif">
           {this.word.theme && (
-            <span className="text-gray-600">{this.word.theme + ' : '}</span>
+            <span className="text-gray-600">{this.word.theme + " : "}</span>
           )}
           {this.word.definition}
         </div>
         <div className="mb-2 ml-2 flex justify-between items-center">
-
           <div className="fixed left-0 ml-2 mb-3 mt-2">
-
-        <button onClick={()=> {this.noteMyWord(note.positif)}}>
-        <FontAwesomeIcon
-            className='text-green-500'
-            icon={faThumbsUp}
-            
-            />
+            <button
+              onClick={() => {
+                this.noteMyWord(note.positif);
+              }}
+            >
+              <FontAwesomeIcon className="text-green-500" icon={faThumbsUp} />
             </button>
             {/*'(' + this.word.positive_note + ')'*/}
-            </div>
-
-        <div className="fixed right-0 mr-2 mb-3 mt-2">
-          <button onClick={()=> {this.noteMyWord(note.negatif)}}>
-        <FontAwesomeIcon
-            className='text-red-500'
-            icon={faThumbsDown}
-            />
-          </button>
-          {/*'(' + this.word.negative_note + ')'*/}
           </div>
-            </div>
 
+          <div className="fixed right-0 mr-2 mb-3 mt-2">
+            <button
+              onClick={() => {
+                this.noteMyWord(note.negatif);
+              }}
+            >
+              <FontAwesomeIcon className="text-red-500" icon={faThumbsDown} />
+            </button>
+            {/*'(' + this.word.negative_note + ')'*/}
+          </div>
+        </div>
       </div>
     );
   }
