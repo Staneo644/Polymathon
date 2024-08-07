f19a9e2c6bdb20b8be44c0aae779703f93b25f62
Stanéo644 <aurele.josserand@gmail.com>
2024-08-07 18:47:49 +0200

    feat: tactile scroll

--- components/cardContainer.tsx
+++ components/cardContainer.tsx
@@ -77,26 +77,59 @@ const CardContainer = (
   const [startIndex, setStartIndex] = useState(0);
   const cardsRefs = useRef<(HTMLDivElement | null)[]>([]);
   const [offset, setOffset] = useState(0);
-
+  const [isFollowing, setIsFollowing] = useState(false);
+  const [clickPoint, setClickPoint] = useState(0);
+  const [position, setPosition] = useState(0);
+  
+  const moveUp = () => {
+    setStartIndex((prevIndex) => {
+      if (prevIndex > 0) {
+        return (prevIndex - 1);
+      }
+      return prevIndex;
+    });
+  }
+  
+  const moveDown = () => {
+    setStartIndex((prevIndex) => {
+      if (prevIndex + 1 < list.length) {
+        if (list.length - prevIndex  == wordLimit - wordNewCall && onEnd)
+          onEnd();
+        return (prevIndex + 1);
+      }
+      return prevIndex;
+    });
+  }
+  
+    const handleMouseMove = (e:any) => {
+      if (isFollowing) {
+        setPosition( clickPoint - e.clientY);
+      }
+    };
+  
+    const handleMouseDown = (e:any) => {
+      setClickPoint(e.clientY);
+      setIsFollowing(true);
+    };
+  
+    const handleMouseUp = () => {
+      if (position < -50) {
+        moveUp();
+      }
+      if (position > 50) {
+        moveDown();
+      }
+      setIsFollowing(false);
+      setPosition(0);
+    };
+    
   useEffect(() => {
     const handleKeyUp = (event:any) => {
       if (event.key === "ArrowDown") {
-        setStartIndex((prevIndex) => {
-          if (prevIndex + 1 < list.length) {
-            if (list.length - prevIndex  == wordLimit - wordNewCall && onEnd)
-              onEnd();
-            return (prevIndex + 1);
-          }
-          return prevIndex;
-        });
+        moveDown();
       }
       if (event.key === 'ArrowUp') {
-        setStartIndex((prevIndex) => {
-          if (prevIndex > 0) {
-            return (prevIndex - 1);
-          }
-          return prevIndex;
-        });
+        moveUp();
       }
     };
 
@@ -118,11 +151,17 @@ const CardContainer = (
   }, [startIndex]);
 
   return (
-    <div className="relative mt-[5px] overflow-hidden" style={{ height: 'calc(100vh - 5px)' }}>
+    <div className="relative mt-[5px] overflow-hidden" style={{ height: 'calc(100vh - 5px)' }}
+          onMouseDown={handleMouseDown}
+          onMouseUp={handleMouseUp}
+          onMouseMove={handleMouseMove}
+          onTouchStart={handleMouseDown}
+          onTouchEnd={handleMouseUp}
+          onTouchMove={handleMouseMove} >
       <div
-        className="transition-transform duration-300 overflow-hidden"
+        className={`${isFollowing ? "" : "transition-transform duration-300"}`}
         style={{
-          transform: `translateY(-${offset}px)`,
+          transform: `translateY(-${offset + position}px)`,
         }}
       >
         {list && list.map((card, index) => (
