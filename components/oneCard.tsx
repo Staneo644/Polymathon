63233d839cf9016464a1177ff3bd002b977f949e
grodriguez-fr <brawlgui99@gmail.com>
2024-08-10 16:48:45 +0200

    fix: like

--- components/oneCard.tsx
+++ components/oneCard.tsx
@@ -22,18 +22,20 @@ type Props = {
   isVisible: boolean;
 };
 
-
-const Explanation = (props: Props) =>
-  <div className="w-100 absolute transition-transform duration-300 mr-2"
-style={{  
-  opacity: `${props.isVisible ? 100 : 0}`,
-  transform: `translateX(${(props.index == 0 ? '-100' : props.index == 2 ? '100' : '0')}vw)`,
-}}>
+const Explanation = (props: Props) => (
+  <div
+    className="w-100 absolute transition-transform duration-300 mr-2"
+    style={{
+      opacity: `${props.isVisible ? 100 : 0}`,
+      transform: `translateX(${props.index == 0 ? "-100" : props.index == 2 ? "100" : "0"}vw)`,
+    }}
+  >
     {props.theme && props.isDefinition && (
       <span className="text-gray-600">{props.theme + " : "}</span>
     )}
     {EtymologyComponent(props.text ?? "")}
   </div>
+);
 /*<FontAwesomeIcon icon={faSlash} className="cursor-pointer -rotate-90 absolute" onClick={handleLeft}/>
     <FontAwesomeIcon icon={faSlash} className="cursor-pointer -rotate-90 -scale-x-100 absolute -translate-x-100" onClick={handleRigth}
       style={
@@ -41,21 +43,23 @@ style={{
       }
     />*/
 
-const Arrow = (props: {directionLeft: boolean, onClick: () => void}) => 
-  
+const Arrow = (props: { directionLeft: boolean; onClick: () => void }) => (
   <button onClick={props.onClick} className="absolute -rotate-90">
-    <FontAwesomeIcon icon={faSlash} 
-    style={
-      {transform: `rotate(-90deg) ${props.directionLeft ? "" : "scaleX(-1) translateX(-99%)"} `}
-    }/>
-    <FontAwesomeIcon icon={faSlash} className="absolute -rotate-90"
-      style={
-        {transform: `rotate(-90deg) ${props.directionLeft ? "scaleX(-1) translateX(-99%)" : ""} `} 
-      }
+    <FontAwesomeIcon
+      icon={faSlash}
+      style={{
+        transform: `rotate(-90deg) ${props.directionLeft ? "" : "scaleX(-1) translateX(-99%)"} `,
+      }}
+    />
+    <FontAwesomeIcon
+      icon={faSlash}
+      className="absolute -rotate-90"
+      style={{
+        transform: `rotate(-90deg) ${props.directionLeft ? "scaleX(-1) translateX(-99%)" : ""} `,
+      }}
     />
   </button>
-  
-
+);
 
 export const likeWordAPI = (id: number, like: boolean | null) =>
   fetch("http://localhost:3000/api/like?id=" + id + "&like=" + like, {
@@ -79,7 +83,6 @@ const OneCard = forwardRef<
   const [numberLikes, setNumberLikes] = useState(props.word.likes);
   const [numberDislikes, setNumberDislikes] = useState(props.word.dislikes);
 
-<<<<<<< HEAD
   const likeWord = async (like: boolean) => {
     if (like) {
       if (currentLike == true) {
@@ -89,7 +92,7 @@ const OneCard = forwardRef<
         });
       } else {
         likeWordAPI(props.word.id, true).then(() => {
-          if (currentLike === false) setNumberDislikes(numberDislikes - 1);
+          if (currentLike == false) setNumberDislikes(numberDislikes - 1);
           setCurrentLike(true);
           setNumberLikes(numberLikes + 1);
         });
@@ -102,47 +105,10 @@ const OneCard = forwardRef<
         });
       } else {
         likeWordAPI(props.word.id, false).then(() => {
-          if (currentLike === true) setNumberLikes(numberLikes - 1);
+          if (currentLike == true) setNumberLikes(numberLikes - 1);
           setCurrentLike(false);
           setNumberDislikes(numberDislikes + 1);
         });
-=======
-    
-    const likeWord = async (like: boolean) => {
-      if (like) {
-        if (currentLike == true) {
-          likeWordAPI(props.word.id, null).then(() => {
-            setCurrentLike(null);
-            setNumberLikes(numberLikes - 1);
-          });
-        }
-        else {
-          likeWordAPI(props.word.id, true).then(() => {
-            if (currentLike == false) {
-              setNumberDislikes(numberDislikes - 1);
-            }
-            setCurrentLike(true);
-            setNumberLikes(numberLikes + 1);
-          })
-        }
-      }
-      else {
-        if (currentLike == false) {
-          likeWordAPI(props.word.id, null).then(() => {
-            setCurrentLike(null)
-            setNumberDislikes(numberDislikes - 1);
-          });
-        }
-        else {
-          likeWordAPI(props.word.id, false).then(() => {
-            if (currentLike == true) {
-              setNumberLikes(numberLikes - 1);
-            }
-            setCurrentLike(false)
-            setNumberDislikes(numberDislikes + 1);
-          })
-        }
->>>>>>> 74ac8f6fd9e2a082c68b37ccd2a5b16522d00716
       }
     }
   };
@@ -187,7 +153,6 @@ const OneCard = forwardRef<
 
   return (
     <div
-<<<<<<< HEAD
       ref={ref}
       className="bg-white shadow-md rounded-lg p-4 mb-[45px] last:mb-0 text-gray-800"
       style={{
@@ -195,69 +160,6 @@ const OneCard = forwardRef<
           "url(https://s2.qwant.com/thumbr/474x323/7/5/15e7a9bcd784af960fb05e85addd943f5f08a5259bb5803b58a1b3f39473cc/th.jpg?u=https%3A%2F%2Ftse.mm.bing.net%2Fth%3Fid%3DOIP.jytUH6XTOQ7pXAgURy6LYQHaFD%26pid%3DApi&q=0&b=1&p=0&a=0)",
         borderRadius: "20px",
         backgroundSize: "cover",
-=======
-    ref={ref}
-  className="bg-white shadow-md rounded-lg p-4 mb-[45px] last:mb-0 text-gray-800"
-  style={{
-    backgroundImage:
-    "url(https://s2.qwant.com/thumbr/474x323/7/5/15e7a9bcd784af960fb05e85addd943f5f08a5259bb5803b58a1b3f39473cc/th.jpg?u=https%3A%2F%2Ftse.mm.bing.net%2Fth%3Fid%3DOIP.jytUH6XTOQ7pXAgURy6LYQHaFD%26pid%3DApi&q=0&b=1&p=0&a=0)",
-    borderRadius: '20px',
-    backgroundSize: "cover",
-  }}
->
-  <div className="mt-2 ml-2 text-xl italic font-serif">
-  </div>
-  <div className="flex items-center justify-center whitespace-pre">
-    <h3 className="font-bold italic font-serif text-2xl">
-      {props.word.name}
-    </h3>
-    <h4 className="text-lg text-gray-600 italic">
-      {"  ("}
-      {props.word.type}
-      {")"}
-    </h4>
-  </div>
-  <button className={"mb-2 ml-2 text-xl italic font-serif text-left" + ((visibleText) ? " cursor-default" : "")} onClick={() => (setVisibleText(true))}>
-  <div className={`duration-300 transition-transform ${ (visibleText ? " " : " invisible")}`}
-    >
-    <Explanation
-      text={listItems[0]} 
-      theme={props.word.theme} 
-      isDefinition={currentIndex == 1}
-      index={currentIndex == 0 ? 2 : currentIndex - 1}
-      isVisible={currentIndex == 2 || oldIndex == 2}
-    />
-    <Explanation
-      text={listItems[1]} 
-      theme={props.word.theme} 
-      isDefinition={currentIndex == 1}
-      index={currentIndex}
-      isVisible={currentIndex == 1 || oldIndex == 1}
-    />
-    <Explanation
-      text={listItems[2]} 
-      theme={props.word.theme} 
-      isDefinition={currentIndex == 1}
-      index={currentIndex == 2 ? 0 : currentIndex + 1}
-      isVisible={currentIndex == 0 || oldIndex == 0}
-      />
-  </div>
-    <div className='invisible'>
-
-  {biggerItem}
-    </div>
-    <Arrow directionLeft={true} onClick={handleLeft}/>
-    <Arrow directionLeft={false} onClick={handleRigth}/>
-  </button>
-  {
-    props.hideLikesDislikes ? null :
-  <div className="mb-2 ml-2 flex justify-between items-center">
-    <div className="relative left-0 ml-2 mb-2 mt-2">
-      <button
-      className="p-1"
-      onClick={() => {
-        likeWord(true);
->>>>>>> 74ac8f6fd9e2a082c68b37ccd2a5b16522d00716
       }}
     >
       <div className="mt-2 ml-2 text-xl italic font-serif"></div>
@@ -304,16 +206,8 @@ const OneCard = forwardRef<
           />
         </div>
         <div className="invisible">{biggerItem}</div>
-        <FontAwesomeIcon
-          icon={faSlash}
-          className="cursor-pointer -rotate-90"
-          onClick={handleLeft}
-        />
-        <FontAwesomeIcon
-          icon={faSlash}
-          className="cursor-pointer -rotate-90 -scale-100 skew-y-12"
-          onClick={handleRigth}
-        />
+        <Arrow directionLeft={true} onClick={handleLeft} />
+        <Arrow directionLeft={false} onClick={handleRigth} />
       </button>
       {props.hideLikesDislikes ? null : (
         <div className="mb-2 ml-2 flex justify-between items-center">
