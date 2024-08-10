4b50fa1f0e091bbab58e37fae742e58205f01ce1
grodriguez-fr <brawlgui99@gmail.com>
2024-08-10 16:53:01 +0200

    feat: /api/admin to know if your admin

---
+++ app/api/admin/route.ts
@@ -0,0 +1,11 @@
+import { getProfile } from "@/utils/profile/getProfile";
+import { createClient } from "@/utils/supabase/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const supabase = createClient();
+
+  const res = await getProfile(supabase);
+  if (res.error || !res.data) return NextResponse.json({ error: res.error });
+  return NextResponse.json({ admin: res.data.admin });
+}
