79ba12859b558df13fd0c67ae6fcb42ad36b3ab5
Guillaume Rodriguez <guillaume.rodriguez@epita.fr>
2024-08-15 14:42:45 +0200

    get popular words

--- app/api/word/popular/route.ts
+++ app/api/word/popular/route.ts
@@ -1,12 +1,5 @@
-import { getLikes } from "@/utils/like/getLikes";
-import { getProfile } from "@/utils/profile/getProfile";
 import { createClient } from "@/utils/supabase/server";
-import { getTheme, getThemes } from "@/utils/theme/getThemes";
-import { getViews } from "@/utils/view/getViews";
-import { enrichWord } from "@/utils/word/enrichWord";
 import { getPopularWords } from "@/utils/word/getPopularWord";
-import { getWorldByThemeLimited } from "@/utils/word/getWorldByTheme";
-import { WordRow } from "@/utils/word/word";
 import { NextRequest, NextResponse } from "next/server";
 
 async function parseParamsGET(
@@ -34,7 +27,6 @@ export interface completeWord {
   last_day_word: string;
 }
 
-//TODO prendre en compte les vues
 /**
  *
  * @param request contient les paramètres de la requête : limit et theme
@@ -43,40 +35,10 @@ export interface completeWord {
 export async function GET(request: NextRequest) {
   const supabase = createClient();
 
-  const themes = await getThemes(supabase);
-  if (themes.error || !themes.data)
-    return NextResponse.json({ error: themes.error });
-
   const { limit, error } = await parseParamsGET(request);
 
   if (error || !limit) return NextResponse.json({ error });
 
-  const res = await getWorldByThemeLimited(supabase, [], undefined, limit);
-
-  if (res.error || !res.data)
-    return NextResponse.json({ error: "db error: " + res.error });
-
-  const likes = await getLikes(supabase, res.data);
-  if (likes.error || !likes.data)
-    return NextResponse.json({ error: likes.error });
-
-  const views = await getViews(supabase, res.data);
-  if (views.error || !views.data)
-    return NextResponse.json({ error: views.error });
-
-  const profile = await getProfile(supabase);
-  if (profile.error || !profile.data)
-    return NextResponse.json({ error: profile.error });
-
-  const final = enrichWord(
-    res.data,
-    themes.data,
-    likes.data,
-    profile.data,
-    views.data
-  );
-
-  const fun = await getPopularWords(supabase);
-  console.log(fun);
-  return NextResponse.json({ data: final });
+  const fun = await getPopularWords(supabase, limit);
+  return NextResponse.json({ data: fun.data });
 }
--- utils/word/getPopularWord.ts
+++ utils/word/getPopularWord.ts
@@ -1,47 +1,10 @@
 import { SupabaseClient } from "@supabase/supabase-js";
 
-export async function getPopularWords(supabase: SupabaseClient) {
-  /*
-  const { data, error } = await supabase
-    .from("word")
-    .select(
-      `
-      id,
-      name,
-      definition,
-      like_count:like!inner (
-        id,
-        created_at,
-        user_id,
-        like
-      )
-    `
-    )
-    .eq("like.like", true)
-    .order("like_count", { ascending: false })
-    .limit(10);
-*/
-  const { data, error } = await supabase
-    .from("word")
-    .select(
-      `
-    id,
-    name,
-    definition,
-    like_count:like(id)
-  `,
-      { count: "exact", head: true }
-    )
-    .eq("like.like", true)
-    .order("like_count", { ascending: false })
-    .limit(10);
-
-  if (error) {
-    console.error("Error fetching data:", error);
-  } else {
-    console.log("Top 10 words with most likes:", data);
-  }
-
-  if (error) return { error };
-  return { data };
+export async function getPopularWords(supabase: SupabaseClient, limit: number) {
+  const select = await supabase.rpc("get_popular_words", {
+    limit_count: limit,
+  });
+  console.log(select);
+  if (select.error) return { error: select.error.message };
+  return { data: select.data };
 }
