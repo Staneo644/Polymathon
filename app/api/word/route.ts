96522461ce1b886e021335391212747588f9704b
Guillaume Rodriguez <guillaume.rodriguez@epita.fr>
2024-08-15 16:37:52 +0200

    chercher un mot par nom et chercher un mot par id

--- app/api/word/route.ts
+++ app/api/word/route.ts
@@ -108,7 +108,6 @@ export async function PATCH(request: NextRequest) {
   }
 
   const profile = await getProfile(supabase);
-
   if (!profile.data || !profile.data.admin) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 403 });
   }
@@ -122,6 +121,26 @@ export async function PATCH(request: NextRequest) {
   if (error) {
     return NextResponse.json(
       { error: "db error: " + error.message },
+      { status: 500 }
+    );
+  }
+
+  // Supprimer les mots avec le meme nom au cas ou ca remplace
+  const select = await supabase.from("word").select().eq("id", id).single();
+  if (select.error) {
+    return NextResponse.json(
+      { error: "db error selecting word by id: " + select.error.message },
+      { status: 500 }
+    );
+  }
+  const deletereq = await supabase
+    .from("word")
+    .delete()
+    .eq("name", select.data.name)
+    .neq("id", id);
+  if (deletereq.error) {
+    return NextResponse.json(
+      { error: "db error deleting word by name: " + deletereq.error.message },
       { status: 500 }
     );
   }
--- utils/word/getWordByName.ts
+++ utils/word/getWordByName.ts
@@ -1,11 +1,7 @@
 import { SupabaseClient } from "@supabase/supabase-js";
 
-export async function getWordByName(supabase: SupabaseClient, name: string) {
-  console.log(name);
-  const { data, error } = await supabase
-    .from("word")
-    .select("*")
-    .eq("name", name);
+export async function getWordById(supabase: SupabaseClient, id: string) {
+  const { data, error } = await supabase.from("word").select().eq("id", id);
 
   if (error) return { error };
   return { data };
