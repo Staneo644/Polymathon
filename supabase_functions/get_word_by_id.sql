55e6a646f443f9719eac96157549a443ba1c5d0d
Guillaume Rodriguez <guillaume.rodriguez@epita.fr>
2024-08-17 18:00:32 +0200

    get word by id

---
+++ supabase_functions/get_word_by_id.sql
@@ -0,0 +1,56 @@
+-- Supprimer la fonction existante
+DROP FUNCTION IF EXISTS get_word_by_id(INTEGER);
+
+-- Créer ou remplacer la fonction
+CREATE OR REPLACE FUNCTION get_word_by_id(id_searched INTEGER)
+RETURNS TABLE (
+  id BIGINT,
+  name VARCHAR,
+  definition TEXT,
+  type VARCHAR,
+  etymology TEXT,
+  example TEXT,
+  theme_id BIGINT,
+  theme_name TEXT,
+  last_day_word DATE,
+  likes BIGINT,
+  dislikes BIGINT,
+  user_like_status BOOLEAN,   -- Indicateur de l'état du like (TRUE = liké, FALSE = disliké, NULL = ni l'un ni l'autre)
+  views BIGINT
+) AS $$
+BEGIN
+  RETURN QUERY
+  SELECT
+    w.id,
+    w.name,
+    w.definition,
+    w.type,
+    w.etymology,
+    w.example,
+    w.theme as theme_id,
+    (SELECT theme.name FROM theme WHERE theme.id = w.theme) AS theme_name,
+    w.last_day_word,
+    (SELECT COUNT(*)
+     FROM "like" 
+     WHERE "like".word = w.id 
+       AND "like"."like" = TRUE) AS likes,
+    (SELECT COUNT(*)
+     FROM "like" 
+     WHERE "like".word = w.id 
+       AND "like"."like" = FALSE) AS dislikes,
+    (SELECT l.like 
+     FROM "like" l 
+     WHERE l.word = w.id 
+       AND l.user_id = auth.uid()
+     LIMIT 1) AS user_like_status,
+    (SELECT COUNT(*)
+     FROM "views"
+     WHERE views.word = w.id) AS views
+  FROM
+    word w
+  WHERE w.id = id_searched;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Appel de la fonction
+SELECT * FROM get_word_by_id(10);
--- utils/word/getWordById.ts
+++ utils/word/getWordById.ts
@@ -1,7 +1,9 @@
 import { SupabaseClient } from "@supabase/supabase-js";
 
 export async function getWordById(supabase: SupabaseClient, id: string) {
-  const { data, error } = await supabase.from("word").select().eq("id", id);
+  const { data, error } = await supabase.rpc("get_word_by_id", {
+    id_searched: id,
+  });
 
   if (error) return { error };
   return { data };
